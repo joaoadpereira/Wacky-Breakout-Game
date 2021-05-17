2e3d3b1eebdd972dbc85244e7795f49463ca8571
Joao <joaoadelino.pereira@gmail.com>
2021-05-17 23:14:55 +0300

    Add Menu system

---
+++ Assets/Scripts/Menus/MenuName.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MenuName 
+{
+   MainMenu, 
+   DifficultyMenu,
+   HelpMenu,
+   GamePlayMenu,
+   PauseMenu,
+}
